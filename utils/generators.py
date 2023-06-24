def9476f8313124df3fca05a91b063aa1a0f2ced
jkcas22 <jk@aloop.ch>
2023-06-24 16:16:22 +0200

    plot all stages of images

--- utils/generators.py
+++ utils/generators.py
@@ -114,7 +114,7 @@ def gen_ellipses(rnd=np.random.default_rng(), num_ellipses=500, max_diam=1/10, m
     return params
 
 
-def gen_image(shapes, lines = None, ellipses = None, im_size=160, max_lw=0.15, min_lw=0.1, min_gray=0.5, show_center=False):
+def gen_image(shapes, lines, ellipses, im_size=160, max_lw=0.15, min_lw=0.1, min_gray=0.5, show_center=False):
     """Generates an image with geometric shapes and noise on it
 
     Parameters
@@ -152,28 +152,19 @@ def gen_image(shapes, lines = None, ellipses = None, im_size=160, max_lw=0.15, m
     sha = shapes.copy()
     sha[:,1:4] = sha[:,1:4]*im_size
     sha[:,5] = min_lw+sha[:,5]*(max_lw-min_lw)
-    plt.axis('scaled')
-    plt.axis('off')
-    plt.xlim(0, im_size)
-    plt.ylim(0, im_size)
-    plt.subplots_adjust(bottom=0.0, left=0.0, right=1.0, top=1.0)
-    ax = plt.gca()
-    patches = []
+    
+    eps = ellipses.copy()
+    eps[:,0:4] = eps[:,0:4]*im_size
+    #eps[:,5] = min_lw+eps[:,5]*(max_lw-min_lw)
+    ecoll = EllipseCollection(eps[:,2],eps[:,3],eps[:,4],units='x',offsets=eps[:,0:2],linewidths=min_lw, edgecolors='face',facecolors=np.matmul(eps[:,6].reshape((-1,1)),np.ones((1,3)))*min_gray, zorder=1)
+
+    lns = lines.copy()
+    lns[:,0:4] = lns[:,0:4]*im_size
+    lns[:,4] = min_lw+lns[:,4]*(max_lw-min_lw)
+    lcoll = LineCollection(lns[:,0:4].reshape((len(lns),2,2)),linewidths=lns[:,4], colors=np.matmul(lns[:,5].reshape((-1,1)),np.ones((1,3)))*min_gray, zorder=3)
+
     box = []
-    if ellipses is None:
-        eps = None
-    else:
-        eps = ellipses.copy()
-        eps[:,0:4] = eps[:,0:4]*im_size
-        #eps[:,5] = min_lw+eps[:,5]*(max_lw-min_lw)
-        ax.add_collection(EllipseCollection(eps[:,2],eps[:,3],eps[:,4],units='x',offsets=eps[:,0:2],linewidths=min_lw, edgecolors='face',facecolors=np.matmul(eps[:,6].reshape((-1,1)),np.ones((1,3)))*min_gray, zorder=1))
-    if lines is None:
-        lns = None
-    else:
-        lns = lines.copy()
-        lns[:,0:4] = lns[:,0:4]*im_size
-        lns[:,4] = min_lw+lns[:,4]*(max_lw-min_lw)
-        ax.add_collection(LineCollection(lns[:,0:4].reshape((len(lns),2,2)),linewidths=lns[:,4], colors=np.matmul(lns[:,5].reshape((-1,1)),np.ones((1,3)))*min_gray, zorder=3))
+    patches = []
     for s in sha:
         if s[0] < 3:
             patch=matplotlib.patches.Circle(s[1:3], radius=s[3], lw=s[5], ec=s[6]*np.ones(3)*min_gray, fill=False)
@@ -181,20 +172,38 @@ def gen_image(shapes, lines = None, ellipses = None, im_size=160, max_lw=0.15, m
             patch=matplotlib.patches.RegularPolygon(s[1:3],numVertices=int(s[0]),radius=s[3],orientation=s[4],lw=s[5], ec=s[6]*np.ones(3)*min_gray, fill=False)
         box.append(patch.get_extents().get_points())
         patches.append(patch)
+
     if show_center:
         for s in sha:
             patches.append(matplotlib.patches.Circle(s[1:3], radius=.5, lw=2, ec='0'))
-    ax.add_collection(PatchCollection(patches, match_original=True, zorder=2))
-    fig = plt.gcf()
-    fig.set(figwidth=1, figheight=1, dpi=im_size)
-    fig.canvas.draw()
-    fig.canvas.flush_events()
-    img = np.array(fig.canvas.renderer.buffer_rgba())
-    plt.cla()
-    return (img[:,:,0],sha,lns,eps,np.reshape(box, (-1,4), order='F'))
+
+    pcoll = PatchCollection(patches, match_original=True, zorder=2)
+
+    images = np.ones((4,im_size,im_size))
+    for i in range(4):
+        plt.axis('scaled')
+        plt.axis('off')
+        plt.xlim(0, im_size)
+        plt.ylim(0, im_size)
+        plt.subplots_adjust(bottom=0.0, left=0.0, right=1.0, top=1.0)
+        
+        ax = plt.gca()
+        ax.add_collection(pcoll)
+        if i%2>0: ax.add_collection(ecoll)
+        if i>1: ax.add_collection(lcoll)
+
+        fig = plt.gcf()
+        fig.set(figwidth=1, figheight=1, dpi=im_size)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        img = np.array(fig.canvas.renderer.buffer_rgba())
+        images[i,:,:]=img[:,:,0]
+        plt.cla()
+
+    return (images,sha,np.reshape(box, (-1,4), order='F'))
 
 
-def gen_details(shapes_im_size, shapes, image, rnd=np.random.default_rng(), max_fluct=0):
+def gen_details(shapes_im_size, shapes, images, rnd=np.random.default_rng(), max_fluct=0):
     """Extracts a detail image of each shape from an image
 
     Parameters
@@ -203,8 +212,8 @@ def gen_details(shapes_im_size, shapes, image, rnd=np.random.default_rng(), max_
         The size of the resulting detail images 
     shapes : ndarray
         The list of shapes, i.e. the shape parameters relativ to the size of image
-    image : ndarray
-        The greyscale image containing the shapes
+    images : ndarray
+        A list of greyscale images containing the shapes
     rnd : Generator
         Generator for random numbers representing the fluctuation of the shapes center
     fluct : int
@@ -213,30 +222,32 @@ def gen_details(shapes_im_size, shapes, image, rnd=np.random.default_rng(), max_
     Returns
     -------
     details : ndarray
-        A list of images of size=(shapes_im_size, shapes_im_size), one image per shape
+        A list of list of images of size=(shapes_im_size, shapes_im_size), one image per shape
     """
-    im_size = len(image)
-    radius = shapes_im_size // 2
-    mid_point = shapes[:,1:3].astype(int)
-    if max_fluct>0:
-        mid_point = mid_point+(rnd.integers(0,2*max_fluct+2,size=(len(shapes),2))-(max_fluct+1))
-    mid_point = np.c_[im_size-mid_point[:,1],mid_point[:,0]]
-    lower_left = mid_point-radius
-    upper_right = lower_left+shapes_im_size
-    details = np.full((len(shapes),shapes_im_size,shapes_im_size),255)
-    for i, s in enumerate(details):
-        x_min = lower_left[i,0]-1
-        x_max = upper_right[i,0]-1
-        y_min = lower_left[i,1]
-        y_max = upper_right[i,1]
-        x_min_cut = max(x_min,0)
-        x_max_cut = min(x_max,im_size)
-        y_min_cut = max(y_min,0)
-        y_max_cut = min(y_max,im_size)
-        x_min_off = x_min_cut-x_min
-        x_max_off = x_max-x_max_cut
-        y_min_off = y_min_cut-y_min
-        y_max_off = y_max-y_max_cut
-        s[x_min_off:(shapes_im_size-x_max_off),y_min_off:(shapes_im_size-y_max_off)] = \
-            image[x_min_cut:x_max_cut,y_min_cut:y_max_cut]
+    details = np.full((len(images),len(shapes),shapes_im_size,shapes_im_size),255)
+    for j, image in enumerate(images):
+        im_size = len(image)
+        radius = shapes_im_size // 2
+        mid_point = shapes[:,1:3].astype(int)
+        if max_fluct>0:
+            mid_point = mid_point+(rnd.integers(0,2*max_fluct+2,size=(len(shapes),2))-(max_fluct+1))
+        mid_point = np.c_[im_size-mid_point[:,1],mid_point[:,0]]
+        lower_left = mid_point-radius
+        upper_right = lower_left+shapes_im_size
+        
+        for i, s in enumerate(details[j]):
+            x_min = lower_left[i,0]-1
+            x_max = upper_right[i,0]-1
+            y_min = lower_left[i,1]
+            y_max = upper_right[i,1]
+            x_min_cut = max(x_min,0)
+            x_max_cut = min(x_max,im_size)
+            y_min_cut = max(y_min,0)
+            y_max_cut = min(y_max,im_size)
+            x_min_off = x_min_cut-x_min
+            x_max_off = x_max-x_max_cut
+            y_min_off = y_min_cut-y_min
+            y_max_off = y_max-y_max_cut
+            s[x_min_off:(shapes_im_size-x_max_off),y_min_off:(shapes_im_size-y_max_off)] = \
+                image[x_min_cut:x_max_cut,y_min_cut:y_max_cut]
     return details
